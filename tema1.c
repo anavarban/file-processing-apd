8877d1988e18e5c28fbcca01835280854bdca0fd
anavarban <anavarban11@gmail.com>
2022-11-19 22:26:18 +0200

    cleanup tema1.c + README

--- tema1.c
+++ tema1.c
@@ -6,11 +6,12 @@
 
 #define NAME_SIZE 40
 #define MAX_FILE_NO 3000
-#define MAX_SIZE 500002
+#define MAX_SIZE 500000
 
 pthread_barrier_t barrier;
 pthread_mutex_t mutex;
 
+//structura pentru pasarea argumentelor functiei de thread
 typedef struct {
     char **file_names;
     int file_number, max_exp, num_threads, id, M;
@@ -34,10 +35,14 @@ void *thread_task(void *arg) {
     int start = ID * (double)N / P;
     int end = min((ID + 1) * (double)N / P, N);
 
-    
+    //alias pt index_record-ul mapperului
     int *index_record = (int*)(args->index_record_mat)[ID];
+
+    //daca threadul este mapper, incepe map
+    //(mapperii vor fi primele M threaduri (dupa index))
     if (ID < P) {
         for (int i = start; i < end; i++) {
+            //threadul curent se va ocupa de fisierele dintre start si end
             FILE* input = fopen(args->file_names[i], "r");
             int no_numbers;
             fscanf(input, "%d", &no_numbers);
@@ -47,6 +52,7 @@ void *thread_task(void *arg) {
                 if (crt_number >= 1) {
                     
                     //logica pt verificare putere perfecta
+                    //se bazeaza pe cautare binara
                     for (int q = 2; q <= args->max_exp; q++) {
                         int mid = 0;
                         int upper_bound = (int)sqrt(crt_number);
@@ -55,8 +61,6 @@ void *thread_task(void *arg) {
                             if (crt_number == (int)pow(mid, q)) {
                                 //pthread_mutex_lock(&mutex);
                                 args->partial_lists[ID][q][index_record[q]] = crt_number;
-                                
-                                //printf("partial_list[%d][%d][%d] = %d\n", ID, q, index_record[q], crt_number);
                                 index_record[q]++;
                                 //pthread_mutex_unlock(&mutex);
                                 break;
@@ -66,14 +70,10 @@ void *thread_task(void *arg) {
                                 break;
                             }
                             if (crt_number < (int)pow(mid, q)) {
-                                //pthread_mutex_lock(&mutex);
                                 upper_bound = mid - 1;
-                                //pthread_mutex_unlock(&mutex);
                             }
                             if (crt_number > (int)pow(mid, q)){
-                                //pthread_mutex_lock(&mutex);
                                 k = mid +1;
-                                //pthread_mutex_unlock(&mutex);
                             }
                         }
                     }
@@ -85,48 +85,52 @@ void *thread_task(void *arg) {
         }
     }
     //s-a terminat map
-    //printf("partial_list[%d][%d][%d] = %d\n");
-    //bariera
+    //bariera, pentru ca toate threadurile mapper sa termine
     pthread_barrier_wait(&barrier);
 
+
+    //nu uita sa stergi!!!!
     // for (int i = 2; i < args->max_exp + 2; i++) {
     //     for (int k = 0; k < (args->index_record_mat)[ID][args->id + 2]; k++) {
     //         printf("partial_list[%d][%d][%d] = %d\n", ID, i, k, args->partial_lists[ID][i][k]);
     //     }
     // }
+
+
     //incepe reduce
-    
+
+    //retinem o lista de valori ce au aparut in lista partiala
     int *partial_reduced = calloc(MAX_SIZE, sizeof(int));
     int count = 0;
     for (int i = 0; i < args->M; i++) {
         for (int k = 0; k < (args->index_record_mat)[i][args->id + 2]; k++) {
             int found = 0;
+            //verificam daca valoarea curenta din lista partiala a fost gasita deja
             for (int j = 0; j < count; j++) {
                 if(partial_reduced[j] == args->partial_lists[i][args->id + 2][k]) {
-                    //pthread_mutex_lock(&mutex);
                     found = 1;
                     j = count;
-                    //pthread_mutex_unlock(&mutex);
                 }
             }
+            //daca nu a fost gasita o adaugam si o numaram
             if(!found) {
-                //pthread_mutex_lock(&mutex);
                 partial_reduced[count] = args->partial_lists[i][args->id + 2][k];
                 count++;
-                //pthread_mutex_unlock(&mutex);
             }
         }
     }
+
+    //nu uita sa stergi!!!
     for (int i = 0; i < count; i++) {
         printf("partial_reduced[%d] = %d in thread %d\n", i, partial_reduced[i], ID);
     }
+
+    //afisam rezultatul reduce in fisierul de output aferent
     char out[NAME_SIZE];
     sprintf(out, "out%d.txt", args->id + 2);
     FILE* output = fopen(out, "w");
     fprintf(output, "%d", count);
     fclose(output);
-
-    
 
     pthread_exit(NULL);
 
@@ -141,7 +145,7 @@ int max(int a, int b) {
 }
 
 int main(int argc, char* argv[]) {
-    //get args
+    //preluare argumente
     if (argc < 4) {
         printf("Numar insuficient de parametri.\n Usage: ./tema1 no_mapperi no_reduceri fisier_intrare\n");
         exit(1);
@@ -155,33 +159,34 @@ int main(int argc, char* argv[]) {
     FILE *in = fopen(input_file, "r");
     int file_number;
 
-    //char file_names[MAX_FILE_NO][NAME_SIZE];
+    //array de string-uri ce pastreaza numele fisierelor
+    //de input
 
     char** file_names = calloc(MAX_FILE_NO, sizeof(char*));
     for (int i = 0; i < MAX_FILE_NO; i++) {
         file_names[i] = calloc(NAME_SIZE, sizeof(char));
     }
 
-
-
+    //citim numele fisierelor din input si le salvam
     fscanf(in, "%d", &file_number);
     for (int i = 0; i < file_number; i++) {
         fscanf(in, "%s", file_names[i]);
     }
 
     int max_exp = R + 1;
+    //vom crea thread-uri in numar de maxim(nr_mapperi, nr_reduceri)
     int num_threads = max(M, R);
     pthread_t threads[num_threads];
 
-
     
-    //index record
+    //index record pentru a salva dimensiunea listelor partiale 
+    //ale fiecarui mapper
     int** index_record_mat = calloc(num_threads, sizeof(int*));
     for (int i = 0; i < num_threads; i++){
         index_record_mat[i] = calloc(max_exp + 2, sizeof(int));
     }
 
-    //int partial_lists[M][max_exp + 2][MAX_SIZE];
+    //retinem listele partiale ca tablou tridimensional
 
     int*** partial_lists = calloc(num_threads, sizeof(int**));
     for (int i = 0; i < num_threads; i++) {
@@ -191,7 +196,7 @@ int main(int argc, char* argv[]) {
         }
     }
 
-
+    //initializare bariera
     pthread_barrier_init(&barrier, NULL, num_threads);
     pthread_mutex_init(&mutex, NULL);
 
@@ -200,6 +205,7 @@ int main(int argc, char* argv[]) {
     int r;
     void *status;
     
+    //creare threaduri
     for (int i = 0; i < num_threads; i++) {
         args[i].id = i;
         args[i].file_names = (char**)file_names;
@@ -217,7 +223,7 @@ int main(int argc, char* argv[]) {
 		}
 
     }
-
+    //join threaduri
     for (int i = 0; i < num_threads; i++) {
 		r = pthread_join(threads[i], &status);
 
@@ -228,18 +234,13 @@ int main(int argc, char* argv[]) {
 	}
 
 
+    //eliberare memorie
+
     for (int i = 0; i < MAX_FILE_NO; i++) {
         free(file_names[i]);
     }
     free(file_names);
 
-    // int*** partial_lists = calloc(num_threads, sizeof(int**));
-    // for (int i = 0; i < num_threads; i++) {
-    //     partial_lists[i] = calloc(max_exp + 2, sizeof(int*));
-    //     for (int j = 0; j < max_exp + 2; j++) {
-    //         partial_lists[i][j] = calloc(MAX_SIZE, sizeof(int));
-    //     }
-    // }
 
     for (int i = 0; i < num_threads; i++) {
         for (int j = 0; j < max_exp + 2; j++) {
@@ -249,6 +250,10 @@ int main(int argc, char* argv[]) {
     }
     free(partial_lists);
 
+    for (int i = 0; i < num_threads; i++){
+        free(index_record_mat[i]);
+    }
+    free(index_record_mat);
 
     pthread_barrier_destroy(&barrier);
 	pthread_mutex_destroy(&mutex);
